0c0d5b33d224c78c841757204782b8c8e52216d6
IT24100028 <it24100028@my.sliit.lk>
2025-08-29 14:05:45 +0530

    Add files via upload

---
+++ Lab Sheet 05/IT24100028.r
@@ -0,0 +1,23 @@
+setwd("C:\\Users\\it24100028\\Desktop\\PS\\IT24100028")
+
+Delivery_Times <- read.table("Exercise - Lab 05.txt",header = TRUE,sep=",")
+fix(Delivery_Times)
+attach(Delivery_Times)
+histogram <- hist(Delivery_Time_.minutes.,main="deliver times",breaks = seq(20,70,length=10),right = FALSE)
+
+freq<-histogram$counts
+breaks<-histogram$breaks
+cum.freq <- cumsum(freq)
+new <- c()
+
+for(i in 1:length(breaks)){
+  if(i==1){
+    new[i]<-0
+  }else{
+    new[i]<-cum.freq[i-1]
+  }
+}
+
+plot(breaks,new,type = "l",main="Cumulative Frequency Polygon",xlab="Delivery Time",ylab="Cumulative Frequency",ylim=c(0,max(cum.freq)))
+
+cbind(Upper_Boundary = breaks,CumFreq=new)
